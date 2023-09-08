11f557437f0d49a47fe69b4a3a9c04e9f980586e
Tutku Ince <tutku.ince@outlook.com>
2023-09-09 00:11:17 +0300

    Return Books Component has been created

--- library-app-fe/src/layouts/HomePage/ExploreTopBooks.tsx
+++ library-app-fe/src/layouts/HomePage/ExploreTopBooks.tsx
@@ -1,6 +1,6 @@
 export const ExploreTopBooks = () => {
     return (
-        <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-dark text-white">
             <div className="col-md-5 p-lg-5 mx-auto my-5">
                 <h1 className="display-4 fw-normal">Find your next adventure</h1>
                 <p className="lead fw-normal">Where would you like to go next?</p>
