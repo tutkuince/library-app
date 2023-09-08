1a717c852237edcf1e20eaa24de202ef69ab3c56
Tutku Ince <tutku.ince@outlook.com>
2023-09-08 17:44:40 +0300

    Created Top Books Components

--- library-app-fe/src/layouts/NavbarAndFooter/Navbar.tsx
+++ library-app-fe/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -2,27 +2,30 @@ import React from "react";
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Passion 2 Read</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Search Books</a>
-                        </li>
-                    </ul>
-                    <span className="navbar-text">
+        <header className="site-header sticky-top">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Passion 2 Read</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarText"
+                            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Search Books</a>
+                            </li>
+                        </ul>
+                        <span className="navbar-text">
                         <button className="btn btn-light" type="button">Sign in</button>
                      </span>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     )
 }
