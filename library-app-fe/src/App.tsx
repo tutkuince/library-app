44304833837821c1823b16d9ba0b394f289c6384
Tutku Ince <tutku.ince@outlook.com>
2023-09-08 17:14:27 +0300

    Navigtaion Bar Component has been created

--- library-app-fe/src/App.tsx
+++ library-app-fe/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the fullstack course</h1>
+        <Navbar/>
     </div>
   );
 }
