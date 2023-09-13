c59bf79f5aaf81d5c5134a7e783ca72950a9b979
Tutku Ince <tutku.ince@outlook.com>
2023-09-13 13:34:54 +0300

    library app backend project initializer

---
+++ library-app-be/src/test/java/com/tutkuince/libraryApp/LibraryAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutkuince.libraryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
